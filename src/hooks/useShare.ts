64a3f0a7f6d1a0a9bb97d4394595c73b48a695c9
Ollie <ollierwoodman@gmail.com>
2025-07-21 12:46:52 +0800

    Always show copy button and update sharing to remove title

--- src/hooks/useShare.ts
+++ src/hooks/useShare.ts
@@ -1,6 +1,8 @@
 import { toast } from "sonner";
+import { useTrackSharedResult } from "./useTrackGoals";
 
 const useShare = () => {
+  const trackSharedResults = useTrackSharedResult();
   const canShare = !!navigator.share as boolean;
 
   const copy = async (text: string) => {
@@ -17,13 +19,14 @@ const useShare = () => {
   };
 
   // title is often ignored, url is by default the current page
-  const share = async (text = "", url = "", title = "") => {
+  const share = async (text = "", url = "") => {
     if (canShare) {
       try {
         await navigator.share({
-          title,
-          text,
-          url,
+          text: text,
+          url: url,
+        }).then(() => {
+          trackSharedResults();
         });
       } catch (error) {
         console.error("Error sharing:", error);
@@ -31,7 +34,9 @@ const useShare = () => {
       }
     } else {
       console.error("Web Share API not supported");
-      void copy(url);
+      void copy(url).then(() => {
+        trackSharedResults();
+      });
     }
   };
 
